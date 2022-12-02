ea9fd4ea8632b8aa1b36921ed0ffd1fbc0ea1014
BinitShrestha08 <Binitshrestha24@gmail.com>
2022-12-02 08:47:55 +0545

    Initial Commit

--- Task1/apicalls/settings.py
+++ Task1/apicalls/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -21,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ixc^-71cbx61yep2b*f0z%y*x=$!i_2!n)v&n*e(s5b==oj=6+'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
